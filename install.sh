24dd6ac88c9ac2eee035d38fee64c31f5fe4a73a
Parham Moieni <62774242+pmoieni@users.noreply.github.com>
2023-10-21 20:24:39 +0330

    add hyprland

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
 ln -s ~/.dotfiles/alacritty ~/.config/alacritty
+ln -s ~/.dotfiles/wezterm ~/.config/wezterm
+ln -s ~/.dotfiles/hypr ~/.config/hypr
 ln -s ~/.dotfiles/fish ~/.config/fish
 ln -s ~/.dotfiles/tmux ~/.config/tmux
 ln -s ~/.dotfiles/nvim ~/.config/nvim
 ln -s ~/.dotfiles/nvim-dev ~/.config/nvim-dev
-ln -s ~/.dotfiles/wezterm ~/.config/wezterm
