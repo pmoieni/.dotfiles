c83d9a5fee381d7e17c6b6fbfe5f9c36aaa32f30
Parham Moieni <62774242+pmoieni@users.noreply.github.com>
2023-10-23 14:05:53 +0330

    waybar and hyprland

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,19 +6,7 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-local mux = wezterm.mux
-
-wezterm.on('gui-attached', function(domain)
-	-- maximize all displayed windows on startup
-	local workspace = mux.get_active_workspace()
-	for _, window in ipairs(mux.all_windows()) do
-		if window:get_workspace() == workspace then
-			window:gui_window():maximize()
-		end
-	end
-end)
-
-config.font = wezterm.font('FiraCode Nerd Font')
+config.font = wezterm.font('JetBrains Mono')
 config.font_size = 11.0
 
 config.window_background_opacity = 0.9
@@ -41,6 +29,5 @@ config.disable_default_key_bindings = true
 config.set_environment_variables = {
 	TERM = 'xterm-256color'
 }
-
 
 return config
