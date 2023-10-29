4c5b698e5939c50370859312fb8522de6c03a111
Parham Moieni <62774242+pmoieni@users.noreply.github.com>
2023-10-29 23:52:34 +0330

    new changes

--- nvim/lua/pmoieni/plugins/theme.lua
+++ nvim/lua/pmoieni/plugins/theme.lua
@@ -1,46 +1,12 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		require("catppuccin").setup({
-			flavour = "mocha",
-			transparent_background = true,
-			integrations = {
-				cmp = true,
-				gitsigns = true,
-				treesitter = true,
-				mini = true,
-				mason = true,
-				dap = {
-					enabled = true,
-					enable_ui = true,
-				},
-				native_lsp = {
-					enabled = true,
-					virtual_text = {
-						errors = { "italic" },
-						hints = { "italic" },
-						warnings = { "italic" },
-						information = { "italic" },
-					},
-					underlines = {
-						errors = { "underline" },
-						hints = { "underline" },
-						warnings = { "underline" },
-						information = { "underline" },
-					},
-					inlay_hints = {
-						background = true,
-					},
-				},
-				telescope = {
-					enabled = true,
-				},
-				which_key = true,
-			},
-		})
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    config = function()
+        require("rose-pine").setup({
+            variant = "dark",
+            disable_background = true,
+        })
 
-		vim.cmd([[colorscheme catppuccin]])
-	end
+        vim.cmd('colorscheme rose-pine')
+    end
 }
