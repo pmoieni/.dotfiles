78fb13fecc83adcc260fe13991cabfcbd6f116bc
Parham Moieni <62774242+pmoieni@users.noreply.github.com>
2023-10-30 00:45:57 +0330

    more fixes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require 'wezterm'
 local config = {}
 
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+    config = wezterm.config_builder()
 end
 
 config.font = wezterm.font('JetBrains Mono')
@@ -11,21 +11,21 @@ config.font_size = 11.0
 
 config.window_background_opacity = 0.8
 
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = 'rose-pine'
 
 config.default_prog = { '/usr/bin/fish', '-l', '-c', 'tmux new-session -A' }
 
 config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
 }
 
 config.enable_tab_bar = false
 
 config.set_environment_variables = {
-	TERM = 'xterm-256color'
+    TERM = 'xterm-256color'
 }
 
 return config
