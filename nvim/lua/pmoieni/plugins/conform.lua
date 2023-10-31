8297ca56f69b89dcfdba91ed292fe37229e856eb
Parham Moieni <62774242+pmoieni@users.noreply.github.com>
2023-10-31 13:11:56 +0330

    fixessss again

--- nvim/lua/pmoieni/plugins/conform.lua
+++ nvim/lua/pmoieni/plugins/conform.lua
@@ -1,10 +1,12 @@
 return {
 	"stevearc/conform.nvim",
 	dependencies = { "mason.nvim" },
-	lazy = true,
-	cmd = "ConformInfo",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
 	config = function()
-		require("conform").setup({
+		local conform = require("conform")
+
+		conform.setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
 				javascript = { { "prettierd", "prettier" } },
@@ -22,5 +24,20 @@ return {
 				lsp_fallback = true,
 			},
 		})
+
+		vim.api.nvim_create_user_command("Format", function(args)
+			local range = nil
+			if args.count ~= -1 then
+				local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+				range = {
+					start = { args.line1, 0 },
+					["end"] = { args.line2, end_line:len() },
+				}
+			end
+			conform.format({ async = true, lsp_fallback = true, range = range })
+		end, { range = true })
+	end,
+	init = function()
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
 	end,
 }
--- nvim/lua/pmoieni/plugins/lsp.lua
+++ nvim/lua/pmoieni/plugins/lsp.lua
@@ -1,147 +1,130 @@
 return {
-    {
-        "VonHeikemen/lsp-zero.nvim",
-        branch = "v3.x",
-        lazy = true,
-        config = false,
-        init = function()
-            vim.g.lsp_zero_extend_cmp = 0
-            vim.g.lsp_zero_extend_lspconfig = 0
-            vim.diagnostic.config({
-                virtual_text = false,
-                severity_sort = true,
-                float = {
-                    style = "minimal",
-                    border = "rounded",
-                    source = "always",
-                    header = "",
-                    prefix = "",
-                },
-            })
-        end,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        cmd = { "LspInfo", "LspInstall", "LspStart" },
-        event = { "BufReadPre", "BufNewFile" },
-        dependencies = {
-            "folke/neodev.nvim",
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-            "hrsh7th/cmp-nvim-lsp",
-        },
-        config = function()
-            require("neodev").setup()
+	{
+		"neovim/nvim-lspconfig",
+		cmd = { "LspInfo", "LspInstall", "LspStart" },
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"folke/neodev.nvim",
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		config = function()
+			require("neodev").setup()
 
-            local lsp = require("lsp-zero")
-            local lspconfig = require("lspconfig")
+			local lspconfig = require("lspconfig")
 
-            lsp.set_sign_icons({
-                error = "",
-                warn = "",
-                info = "",
-                hint = "",
-            })
+			local lsp_defaults = lspconfig.util.default_config
+			lsp_defaults.capabilities =
+				vim.tbl_deep_extend("force", lsp_defaults.capabilities, require("cmp_nvim_lsp").default_capabilities())
 
-            lsp.extend_lspconfig()
+			vim.diagnostic.config({
+				virtual_text = false,
+				severity_sort = true,
+				float = {
+					style = "minimal",
+					border = "rounded",
+					source = "always",
+					header = "",
+					prefix = "",
+				},
+			})
 
-            lsp.on_attach(function(_, bufnr)
-                lsp.buffer_autoformat()
+			vim.api.nvim_create_autocmd("LspAttach", {
+				desc = "LSP actions",
+				callback = function(event)
+					local keymap = vim.keymap -- for conciseness
 
-                local keymap = vim.keymap -- for conciseness
+					local opts = { noremap = true, silent = false, buffer = event.bufnr }
 
-                local opts = { noremap = true, silent = false, buffer = bufnr }
+					-- set keybinds
+					opts.desc = "Show LSP references"
+					keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
 
-                -- set keybinds
-                opts.desc = "Show LSP references"
-                keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+					opts.desc = "Go to declaration"
+					keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
 
-                opts.desc = "Go to declaration"
-                keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+					opts.desc = "Show LSP definitions"
+					keymap.set("n", "gd", vim.lsp.buf.definition, opts) -- show lsp definitions
 
-                opts.desc = "Show LSP definitions"
-                keymap.set("n", "gd", vim.lsp.buf.definition, opts) -- show lsp definitions
+					opts.desc = "Show LSP implementations"
+					keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
 
-                opts.desc = "Show LSP implementations"
-                keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+					opts.desc = "Show LSP type definitions"
+					keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
-                opts.desc = "Show LSP type definitions"
-                keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+					opts.desc = "See available code actions"
+					keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
-                opts.desc = "See available code actions"
-                keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+					opts.desc = "Smart rename"
+					keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
 
-                opts.desc = "Smart rename"
-                keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+					opts.desc = "Show diagnostics"
+					keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics<CR>", opts) -- show  diagnostics for file
 
-                opts.desc = "Show diagnostics"
-                keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics<CR>", opts) -- show  diagnostics for file
+					opts.desc = "Show line diagnostics"
+					keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
-                opts.desc = "Show line diagnostics"
-                keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+					opts.desc = "Go to previous diagnostic"
+					keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
 
-                opts.desc = "Go to previous diagnostic"
-                keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+					opts.desc = "Go to next diagnostic"
+					keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
 
-                opts.desc = "Go to next diagnostic"
-                keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+					opts.desc = "Show documentation for what is under cursor"
+					keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
-                opts.desc = "Show documentation for what is under cursor"
-                keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+					opts.desc = "Restart LSP"
+					keymap.set("n", "<leader>rs", "<cmd>LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+				end,
+			})
 
-                opts.desc = "Restart LSP"
-                keymap.set("n", "<leader>rs", "<cmd>LspRestart<CR>", opts) -- mapping to restart lsp if necessary
-            end)
+			local default_setup = function(server)
+				lspconfig[server].setup({})
+			end
 
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "tsserver",
-                    "rust_analyzer",
-                    "gopls",
-                    "lua_ls",
-                    "pylsp",
-                },
-                handlers = {
-                    lsp.default_setup,
-                    lua_ls = function()
-                        local lua_opts = lsp.nvim_lua_ls()
-                        lspconfig.lua_ls.setup(lua_opts)
-                    end,
-                },
-            })
-        end,
-    },
-    {
-        "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
-        dependencies = {
-            { "L3MON4D3/LuaSnip" },
-        },
-        config = function()
-            local lsp = require("lsp-zero")
-            local cmp = require("cmp")
-            local cmp_action = lsp.cmp_action()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"tsserver",
+					"rust_analyzer",
+					"gopls",
+					"lua_ls",
+					"pylsp",
+				},
+				handlers = { default_setup },
+			})
+		end,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			{ "L3MON4D3/LuaSnip" },
+		},
+		config = function()
+			local cmp = require("cmp")
 
-            lsp.extend_cmp()
+			cmp.setup({
+				sources = {
+					{ name = "nvim_lsp" },
+				},
+				mapping = cmp.mapping.preset.insert({
+					-- `Enter` key to confirm completion
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
 
-            cmp.setup({
-                formatting = lsp.cmp_format(),
-                mapping = cmp.mapping.preset.insert({
-                    -- `Enter` key to confirm completion
-                    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+					-- Ctrl+Space to trigger completion menu
+					["<C-Space>"] = cmp.mapping.complete(),
 
-                    -- Ctrl+Space to trigger completion menu
-                    ["<C-Space>"] = cmp.mapping.complete(),
-
-                    -- Navigate between snippet placeholder
-                    ["<C-f>"] = cmp_action.luasnip_jump_forward(),
-                    ["<C-b>"] = cmp_action.luasnip_jump_backward(),
-
-                    -- Scroll up and down in the completion documentation
-                    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-d>"] = cmp.mapping.scroll_docs(4),
-                }),
-            })
-        end,
-    },
+					-- Scroll up and down in the completion documentation
+					["<C-u>"] = cmp.mapping.scroll_docs(-4),
+					["<C-d>"] = cmp.mapping.scroll_docs(4),
+				}),
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+			})
+		end,
+	},
 }
